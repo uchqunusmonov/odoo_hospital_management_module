9d9248a03863097a34f0e1f17f05c05c2c78c6d3
uchqunusmonov <usmonov_uchqun@mail.ru>
2024-04-18 13:03:34 +0500

    created new module hospital management

---
+++ custom_addons/hospital_management/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital Management System',
+    'description': """Hospital Management""",
+    'sequence': -100,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
